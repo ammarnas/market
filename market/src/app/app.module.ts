44bd6750dc5f811ffbb62c4dfe64163a8acccae4
ammarnas <ammarnasser.contact@gmail.com>
2023-02-12 15:00:24 +0300

    cart done

--- market/src/app/app.module.ts
+++ market/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartsModule } from './carts/carts.module';
 import { ProductsModule } from './products/products.module';
 
 import { SharedModule } from './shared/shared.module';
@@ -18,6 +19,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     ProductsModule,
+    CartsModule,
     SharedModule,
   ],
   providers: [],
--- market/src/app/carts/services/carts.service.ts
+++ market/src/app/carts/services/carts.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  CreateNewCart(model: any) {
+    return this.http.post(environment.baseAPI + "carts" , model);
+  }
 }
